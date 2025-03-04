91aeed9dfdccb5e5f02e247b13808c53aef923b8
Yuriy <y.gavrinskiy@gmail.com>
2025-03-04 21:36:20 +0300

    add homework 4

--- ter-homeworks-04/src/main.tf
+++ ter-homeworks-04/src/main.tf
@@ -1,23 +1,31 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
-}
-
 data "yandex_compute_image" "ubuntu" {
   family = var.os_image_name
+}
+
+module "vpc_prod" {
+  source   = "./vpc"
+  env_name = "production"
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-d", cidr = "10.0.3.0/24" },
+  ]
+}
+
+module "vpc_dev" {
+  source   = "./vpc"
+  env_name = "develop"
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+  ]
 }
 
 module "marketing_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = [var.default_zone]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_ids_map["ru-central1-a"]]
   instance_name  = var.remote_module_resources.marketing.instance_name
   instance_count = var.remote_module_resources.marketing.instance_count
   image_family   = var.os_image_name
@@ -33,9 +41,9 @@ module "marketing_vm" {
 module "analytics_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = [var.default_zone]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_ids_map["ru-central1-a"]]
   instance_name  = var.remote_module_resources.analytics.instance_name
   instance_count = var.remote_module_resources.analytics.instance_count
   image_family   = var.os_image_name
